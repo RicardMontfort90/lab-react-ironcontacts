071010a55ff337ebcdf5b1ddcdf4364e33b3cef7
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-02 12:41:38 +0100

    Iteration 1 | Display 5 Contacts Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import React, { useState } from "react";
+import contacts from "./contacts.json";
 import './App.css';
 
 function App() {
+  const [contactList] = useState(contacts.slice(0, 5));
+
+
   return (
-    <div className="App"></div>
+    <div>
+      <h1>Contacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactList.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img src={contact.pictureUrl} alt={contact.name} />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
+
 
 export default App;
